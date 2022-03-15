5f9b5bfb35c5cc1e346fbb201e239bcf8ba9088b
TobiasHBrandt <39334625+TobiasHBrandt@users.noreply.github.com>
2022-03-15 10:12:45 +0100

    setup

--- my-react-app/src/Home.js
+++ my-react-app/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -10,10 +10,17 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
 
+    const [name2, setName2] = useState('mario');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
+
+    useEffect(() => {
+        console.log('use effect');
+        console.log(name2);
+    }, [ name2 ]);
 
     const [name, setName] = useState('mario');
     const [age, setAge] = useState(25);
@@ -30,6 +37,8 @@ const Home = () => {
             <button onClick={handleClick}>Klik her</button>
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
             <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs"/>
+            <button onClick={() => setName2('luigi')}>change name</button>
+            <p> { name2 }</p>
         </div>
      );
 }
