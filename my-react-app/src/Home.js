233c19fee7cadb10f04587d4a6f72db5336b65b8
TobiasHBrandt <39334625+TobiasHBrandt@users.noreply.github.com>
2022-03-15 13:15:14 +0100

    loading time

--- my-react-app/src/Home.js
+++ my-react-app/src/Home.js
@@ -4,21 +4,27 @@ import BlogList from "./BlogList";
 const Home = () => {
 
     //let name = 'mario';
+    // make a usestate
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true)
 
     const [name2, setName2] = useState('mario');
 
    
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-            return res.json()
-        })
-        .then(data => {
-            console.log(data);
-            setBlogs(data)
-        });
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                console.log(data);
+                setBlogs(data);
+                setIsPending(false);
+            });
+        }, 1000);
+       
     }, []);
 
     const [name, setName] = useState('mario');
@@ -34,6 +40,8 @@ const Home = () => {
             <h2>Homepage</h2>
             <p>{ name } is { age } years old</p>
             <button onClick={handleClick}>Klik her</button>
+
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
             {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs"/> */}
             <button onClick={() => setName2('luigi')}>change name</button>
