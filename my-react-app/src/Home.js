89b85c26b3407ed7ed823558f647d7c8c5d68c64
TobiasHBrandt <39334625+TobiasHBrandt@users.noreply.github.com>
2022-03-21 14:59:17 +0100

    move useeffect to usefetch json file

--- my-react-app/src/Home.js
+++ my-react-app/src/Home.js
@@ -1,40 +1,13 @@
 import { useState, useEffect} from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
 
-    //let name = 'mario';
+    const { data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
     // make a usestate
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
     const [name2, setName2] = useState('mario');
 
-   
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-            .then(res => {
-                console.log(res)
-                if (!res.ok) {
-                    throw Error('could not fetch the bacon for that resource')
-                }
-                return res.json()
-            })
-            .then(data => {
-                setBlogs(data);
-                setIsPending(false);
-                setError(null);
-            })
-            .catch(err => {
-                setIsPending(false);
-                setError(err.message);
-            })
-        }, 1000);
-       
-    }, []);
 
     const [name, setName] = useState('mario');
     const [age, setAge] = useState(25);
